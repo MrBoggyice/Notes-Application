95278c2df403a9242f2f8e767881022393b83ccc
fortune <legalmody@gmail.com>
2016-08-17 11:42:13 +0100

    constructed NotesApplication

---
+++ notesApp.js
@@ -0,0 +1,36 @@
+class NotesApplication {
+	constructor(author){
+		this.author = author;
+		this.notes = [];
+	}
+
+	// Creates a note and pushes to note list
+	create(note_content){
+
+	}
+
+	// List the available notes
+	listNotes(){
+
+	}
+
+	// Gets a note with its id
+	getANote(note_id){
+
+	}
+
+	// Searches note with a search text
+	search(search_text){
+
+	}
+
+	// Deletes a note with its ID
+	delete(note_id){
+
+	}
+
+	// Updates a note
+	edit(note_id, new_content){
+
+	}
+}
