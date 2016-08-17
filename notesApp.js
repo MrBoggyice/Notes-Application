a5800876f56966a3ff2c6dcb8b6b71239ea9ce47
fortune <legalmody@gmail.com>
2016-08-17 14:50:39 +0100

    implemented create, list and get methods

--- notesApp.js
+++ notesApp.js
@@ -1,32 +1,51 @@
-class NotesApplication {
-	constructor(author){
+class Notes{
+	constructor(note, author){
+		this.note = note;
 		this.author = author;
+	}
+}
+
+class NotesApplication {
+	constructor{
 		this.notes = [];
 	}
 
 	// Creates a note and pushes to note list
-	create(note_content){
-		this.notes.push(note_content);
+	create(note_content, author){
+		var note = new Notes(note_content, author);
+		this.notes.push(note);
 	}
 
 	// List the available notes
 	listNotes(){
-		
+		for(var i = 0; i < this.notes.length; i++) {
+	      console.log("Note ID: " + this.notes.indexOf(i));
+	      console.log(this.notes[i] + "\n");
+	      console.log("By Author " + this.notes[i].author + "\n\n");
+	    }
 	}
 
+
 	// Gets a note with its id
-	getANote(note_id){
+	get(note_id){
+		for(var i = 0; i < this.notes.length; i++){
+			if(note_id === this.notes.indexOf(this.notes[i])){
+				return this.notes[i];
+			}else{
+				return 'no note with such id';
+			}
+		}
 
 	}
 
 	// Searches note with a search text
 	search(search_text){
-
+		
 	}
 
 	// Deletes a note with its ID
 	delete(note_id){
-
+		
 	}
 
 	// Updates a note
